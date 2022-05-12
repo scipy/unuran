a63d39160e5ecc4402e7ed0e8417f4c3ff9634cb
Tirth Patel <tirthasheshpatel@gmail.com>
2022-05-12 14:20:27 +0530

    Add copyright back (#6)

--- unuran/src/distributions/c_uniform.c
+++ unuran/src/distributions/c_uniform.c
@@ -27,7 +27,9 @@
  *                                                                           *
  *****************************************************************************
  *                                                                           *
-
+ *   Copyright (c) 2000-2022 Wolfgang Hoermann and Josef Leydold             *
+ *   Department of Statistics and Mathematics, WU Wien, Austria              *
+ *   SPDX-License-Identifier: BSD-3-Clause                                   *
  *                                                                           *
 
  *                                                                           *
